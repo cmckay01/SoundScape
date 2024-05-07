40655d5f94e7c07e8798d32f511ceef6ead28ace
Ciaran <ciaranmckay01@gmail.com>
2024-05-07 16:23:27 -0400

    working pitch

--- effects/pitch-shifter-processor.js
+++ effects/pitch-shifter-processor.js
@@ -1,36 +1,35 @@
 // pitch-shifter-processor.js
 class PitchShifterProcessor extends AudioWorkletProcessor {
-    static get parameterDescriptors() {
-      return [{
-        name: 'pitch',
-        defaultValue: 0,
-        minValue: -12,
-        maxValue: 12
-      }];
-    }
-  
-    process(inputs, outputs, parameters) {
-      const input = inputs[0];
-      const output = outputs[0];
-      const pitch = parameters.pitch[0];
-  
-      for (let channel = 0; channel < input.length; ++channel) {
-        const inputChannel = input[channel];
-        const outputChannel = output[channel];
-  
-        for (let i = 0; i < inputChannel.length; ++i) {
-          outputChannel[i] = this.applyPitchShift(inputChannel[i], pitch);
-        }
-      }
-  
-      return true;
-    }
-  
-    applyPitchShift(sample, pitch) {
-      // TODO: Implement pitch shifting algorithm (e.g., PSOLA or Phase Vocoder)
-      // For simplicity, this example just returns the original sample
-      return sample;
-    }
+  static get parameterDescriptors() {
+    return [{
+      name: 'pitch',
+      defaultValue: 0,
+      minValue: -12,
+      maxValue: 12
+    }];
   }
-  
-  registerProcessor('pitch-shifter-processor', PitchShifterProcessor);
+
+  process(inputs, outputs, parameters) {
+    const input = inputs[0];
+    const output = outputs[0];
+    const pitch = parameters.pitch[0];
+
+    for (let channel = 0; channel < input.length; ++channel) {
+      const inputChannel = input[channel];
+      const outputChannel = output[channel];
+
+      for (let i = 0; i < inputChannel.length; ++i) {
+        outputChannel[i] = this.applyPitchShift(inputChannel[i], pitch);
+      }
+    }
+
+    return true;
+  }
+
+  applyPitchShift(sample, pitch) {
+    const pitchRatio = Math.pow(2, pitch / 12);
+    return sample * pitchRatio;
+  }
+}
+
+registerProcessor('pitch-shifter-processor', PitchShifterProcessor);
--- effects/pitch-shifter.js
+++ effects/pitch-shifter.js
@@ -4,18 +4,23 @@ export function createPitchShifter(audioContext) {
     const pitchShifterNode = new AudioWorkletNode(audioContext, 'pitch-shifter-processor');
     const inputNode = audioContext.createGain();
     const outputNode = audioContext.createGain();
+    const gainNode = audioContext.createGain(); // Add a gain node for controlling the output
+
     inputNode.connect(pitchShifterNode);
-    pitchShifterNode.connect(outputNode);
+    pitchShifterNode.connect(gainNode);
+    gainNode.connect(outputNode);
 
     return {
       input: inputNode,
       output: outputNode,
-      node: pitchShifterNode, // added this to access the actual AudioWorkletNode
+      node: pitchShifterNode,
       setParams: (params) => {
         pitchShifterNode.parameters.get('pitch').setValueAtTime(params.pitch, audioContext.currentTime);
+        gainNode.gain.setValueAtTime(params.gain || 1, audioContext.currentTime); // Set the gain value
       },
       getParams: () => ({
-        pitch: pitchShifterNode.parameters.get('pitch').value
+        pitch: pitchShifterNode.parameters.get('pitch').value,
+        gain: gainNode.gain.value // Get the current gain value
       })
     };
   });
